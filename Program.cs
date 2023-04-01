c527976b31eac01fb8aa9b2407ea5b493ba121c0
Brenno Prettyman <brennoprettyman@gmail.com>
2023-04-01 17:22:52 -0300

    Concluido

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿namespace Full_Name;
+class Program
+{
+    static void Main(string[] args)
+    {
+        
+        Console.Clear();
+        
+        String nome, sobrenome, Nomecompleto, Nomedecatalogo;
+
+        Console.WriteLine("Digite seu nome: ");
+        nome = Console.ReadLine()!;
+
+    Console.WriteLine();
+
+        Console.WriteLine("Digite seu sobrenome: ");
+        sobrenome = Console.ReadLine()!;
+
+    Console.Clear();
+
+    Console.WriteLine();
+    Nomecompleto = nome + " " + sobrenome;
+    Nomedecatalogo = sobrenome + ", " + nome;
+
+         Console.WriteLine($"Seu nome Completo é: {Nomecompleto}"); 
+         Console.WriteLine();   
+         Console.WriteLine($"Seu nome de Catalógo é: {Nomedecatalogo.ToUpper()}"); 
+
+
+
+
+        
+        
+    }
+}
